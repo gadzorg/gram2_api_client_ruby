ef2fbb8250fb2905926c6ceac53f91859a9932f1
blaked <dorian.becker@gadz.org>
2016-07-08 13:54:03 +0200

    remove json_api

--- lib/gram_v2_client/group.rb
+++ lib/gram_v2_client/group.rb
@@ -1,4 +1,3 @@
-require 'json_api_client'
 require 'gram_v2_client/base'
 
 class GramV2Client::Group < GramV2Client::Base
--- lib/gram_v2_client/role.rb
+++ lib/gram_v2_client/role.rb
@@ -1,4 +1,3 @@
-require 'json_api_client'
 require 'gram_v2_client/base'
 
 class GramV2Client::Role < GramV2Client::Base
